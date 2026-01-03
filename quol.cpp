73c05f027584cbdf8b3ba9a8c0df4b48c649e6b9
kh-lu2 <huhiak1@gmail.com>
2026-01-03 19:58:14 +0100

    add fraction expansion

--- quol.cpp
+++ quol.cpp
@@ -4,6 +4,7 @@
 #include <queue>
 #include <cmath>
 #include <map>
+#include <numeric>
 using namespace std;
 
 typedef complex<double> CD;
@@ -195,64 +196,104 @@ struct Circuit {
     }
 };
 
+int phase_estimation()  {
+    Circuit sus(8);
+    sus.add_gate<NotGate>({4});
 
-int main() {
-    int z = 30;
-    map<int, int> counts;
-    while (z--)
-    {    
-        Circuit sus(8);
-        sus.add_gate<NotGate>({4});
+    for (int qubit = 0; qubit < 4; qubit++)
+        sus.add_gate<HadamardGate>({qubit});
 
-        for (int qubit = 0; qubit < 4; qubit++)
-            sus.add_gate<HadamardGate>({qubit});
+    Operator U;
+    U.add_gate<SwapGate>({4, 5});
+    U.add_gate<SwapGate>({5, 6});
+    U.add_gate<SwapGate>({6, 7});
+    U.add_gate<NotGate>({4});
+    U.add_gate<NotGate>({5});
+    U.add_gate<NotGate>({6});
+    U.add_gate<NotGate>({7});
 
-        Operator U;
-        U.add_gate<SwapGate>({4, 5});
-        U.add_gate<SwapGate>({5, 6});
-        U.add_gate<SwapGate>({6, 7});
-        U.add_gate<NotGate>({4});
-        U.add_gate<NotGate>({5});
-        U.add_gate<NotGate>({6});
-        U.add_gate<NotGate>({7});
-
-        for (int i = 0; i < 4; i++) {
-            int reps = 1 << i;
-            for (int r = 0; r < reps; r++) {
-                for (int g = 0; g < U.gates.size(); g++) {
-                    MCD controlled_matrix = createControlledMatrix(U.gates[g]->matrix);
-                    vector<int> controlled_indexes = {i};
-                    for (auto &idx : U.gate_indexes[g])
-                        controlled_indexes.push_back(idx);
-                    sus.add_gate<Gate>(controlled_indexes, controlled_matrix);
-                }
+    for (int i = 0; i < 4; i++) {
+        int reps = 1 << i;
+        for (int r = 0; r < reps; r++) {
+            for (int g = 0; g < U.gates.size(); g++) {
+                MCD controlled_matrix = createControlledMatrix(U.gates[g]->matrix);
+                vector<int> controlled_indexes = {i};
+                for (auto &idx : U.gate_indexes[g])
+                    controlled_indexes.push_back(idx);
+                sus.add_gate<Gate>(controlled_indexes, controlled_matrix);
             }
         }
-
-        for (int qubit = 3; qubit >= 0; qubit--) {
-            sus.add_gate<HadamardGate>({qubit});
-            
-            for (int qubit2 = qubit - 1; qubit2 >= 0; qubit2--) {
-                double theta = M_PI / (1 << (qubit - qubit2));
-                MCD phase_matrix = {{1, 0}, {0, CD(polar(1.0, theta))}};
-                MCD controlled_phase = createControlledMatrix(phase_matrix);
-                sus.add_gate<Gate>({qubit2, qubit}, controlled_phase);
-            }
-        }
-        
-        sus.add_gate<SwapGate>({0, 3});
-        sus.add_gate<SwapGate>({1, 2});
-
-        sus.run();
-        vector<bool> read = sus.magic_read();
-        int res = 0;
-        for (int i = 0; i < 4; i++)
-            res += (read[i] << i);
-
-        counts[res]++;        
     }
 
-    for (auto& [count, value] : counts)
-        cout << count << ": " << value << endl;
+    for (int qubit = 3; qubit >= 0; qubit--) {
+        sus.add_gate<HadamardGate>({qubit});
+        
+        for (int qubit2 = qubit - 1; qubit2 >= 0; qubit2--) {
+            double theta = M_PI / (1 << (qubit - qubit2));
+            MCD phase_matrix = {{1, 0}, {0, CD(polar(1.0, theta))}};
+            MCD controlled_phase = createControlledMatrix(phase_matrix);
+            sus.add_gate<Gate>({qubit2, qubit}, controlled_phase);
+        }
+    }
+    
+    sus.add_gate<SwapGate>({0, 3});
+    sus.add_gate<SwapGate>({1, 2});
 
+    sus.run();
+    vector<bool> read = sus.magic_read();
+    int res = 0;
+    for (int i = 0; i < 4; i++)
+        res += (read[i] << i);
+
+    return res;       
+}
+
+int get_r_from_fraction_expansion(int p, int n, int a, int m) {
+    int temp_m = m, temp_p = p;
+    vector<int> wholes {0}; 
+
+    while (temp_p > 0) {
+        wholes.push_back(temp_m / temp_p);
+        int mod = temp_m % temp_p;
+        temp_m = temp_p;
+        temp_p = mod;
+    }
+
+    int q_prev = 0, q_prevprev = 1;
+
+    for (int i = 0; i < wholes.size(); i++) {
+        int r = wholes[i] * q_prev + q_prevprev;
+        q_prevprev = q_prev;
+        q_prev = r;
+        if (! (r % 2) && ((int)pow(a, r / 2) % n != n - 1 ) )
+            return r;
+    }
+
+
+    return -1;
+}
+
+void shor(int n = 15, int a = 7, int m = 16) {
+    bool finished = 0;
+    do {
+        int p = phase_estimation();
+        cout << "got " << p << " from phase estimation\n";
+        int r = get_r_from_fraction_expansion(p, n, a, m);
+
+        int q1 = gcd((int)pow(a, r / 2) - 1, n);
+        int q2 = gcd((int)pow(a, r / 2) + 1, n);
+        if (q1 != 1 && q1 != n) {
+            cout << "found divisor! " << q1 << "\n";
+            finished = 1;
+        }
+        if (q2 != 1 && q2 != n) {
+            cout << "found divisor! " << q2 << "\n";
+            finished = 1;
+        }
+    } while (!finished);
+}
+
+
+int main() {
+    shor();
 }
