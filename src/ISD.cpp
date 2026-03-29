ca39de6512af5f0038291f317b73563d248f5d80
kh-lu2 <huhiak1@gmail.com>
2026-03-29 22:11:04 +0200

    solving example equation system

--- src/ISD.cpp
+++ src/ISD.cpp
@@ -4,6 +4,16 @@
 
 void ISD::run_example_system_of_equations() {
     cout << "======================= ISD ======================\n";
+    cout << "Solving the following system of binary equations:\n";
+    cout << "x2 + x3 = 1\n";
+    cout << "x1 + x3 = 0\n";
+    cout << "x1 + x2 + x3 = 1\n\n";
+    cout << "Expected values: x1 = 0, x2 = 1, x3 = 0\n\n";
+
+    cout << "Input matrix:\n";
+    cout << "0 1 1 | 1\n";
+    cout << "1 0 1 | 0\n";
+    cout << "1 1 1 | 1\n\n";
 
     Circuit ISD(12);
     ISD.add_gate<NotMatrix>({1});
@@ -19,6 +29,7 @@ void ISD::run_example_system_of_equations() {
     int n = 3;
 
     for (int i = 0; i < n - 1; i++) {
+        //pivot search
         for (int j = i + 1; j < n; j++) {            
             Operator add_row_op;
             for (int it = i + 1; it < n + 1; it++)
@@ -43,12 +54,31 @@ void ISD::run_example_system_of_equations() {
             for (int it = i; it < j; it++)
                 ISD.add_gate<NotMatrix>({i * n + it});
         }
+
+        //row reduce
+        for (int j = i + 1; j < n; j++) {
+            Operator add_row_op;
+            for (int it = i + 1; it < n + 1; it++)
+                add_row_op.add_gate<CNOTMatrix>({it * n + i, it * n + j});
+
+            for (auto const &gate: add_row_op.gates)             
+                ISD.add_gate(createControlledGate(gate, (i * n + j)));
+        }
     }
+
+    //back substitution
+    for (int i = n - 1; i >= 1; i--)
+        for (int j = i - 1; j >= 0; j--)
+            ISD.add_gate<CCNOTMatrix>({i * n + j, n * n + i, n * n + j});
+
 
     ISD.run();
     vector<bool> read = ISD.magic_read();
 
-    for (int i = 0; i < 12; i++) 
-        cout << i << " " << read[i] << "\n"; 
+    cout << "Output matrix:\n";
+    cout << to_string(read[0]) + " " + to_string(read[3]) + " " + to_string(read[6]) + " | " + to_string(read[9]) + "\n";
+    cout << to_string(read[1]) + " " + to_string(read[4]) + " " + to_string(read[7]) + " | " + to_string(read[10]) + "\n";
+    cout << to_string(read[2]) + " " + to_string(read[5]) + " " + to_string(read[8]) + " | " + to_string(read[11]) + "\n\n";
 
+    cout << "Calculated values: x1 = " + to_string(read[9]) + ", x2 = " + to_string(read[10]) + ", x3 = " + to_string(read[11]) + "\n\n";
 }
